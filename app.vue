f3ee76a4784715a13f10a1e335d9d0df4e930942
Wildan Novaldi <wildanv10@gmail.com>
2025-03-29 15:41:37 +0700

    feat: add style and animation to elements
    - add style to form input
    - add style to table
    - add animation to table's rows

--- app.vue
+++ app.vue
@@ -1,16 +1,18 @@
 <script setup>
 import { ref } from "vue";
 
+const isCalculating = ref(false);
 const inputs = ref({
-  hargaProperty: 0,
-  dp: 0,
-  tenor: 0,
-  fixedRate: 0,
-  fixedTenor: 0,
-  floatingRate: 0,
-  floatingTenor: 0,
+  hargaProperty: 500000000,
+  dp: 100000000,
+  tenor: 15,
+  fixedRate: 4,
+  fixedTenor: 3,
+  floatingRate: 11,
+  floatingTenor: 12,
 });
 const hasil = ref([]);
+const hasilBarisRefs = ref([]);
 
 const formatCurrency = (value) => {
   return new Intl.NumberFormat("id-ID", {
@@ -45,125 +47,303 @@ const formatRateInput = (field, event) => {
   inputs.value[field] = value;
 };
 
-const hitungKPR = () => {
+const hitungKPR = async () => {
   const pinjaman = inputs.value.hargaProperty - inputs.value.dp;
   let sisaPokok = pinjaman;
   let bulan = 1;
   hasil.value = [];
+  hasilBarisRefs.value = [];
+  isCalculating.value = true;
+  await nextTick();
 
-  for (let tahun = 1; tahun <= inputs.value.tenor; tahun++) {
-    const rate =
-      tahun <= inputs.value.fixedTenor
-        ? inputs.value.fixedRate / 100 / 12
-        : tahun <= inputs.value.fixedTenor + inputs.value.floatingTenor
-        ? inputs.value.floatingRate / 100 / 12
-        : inputs.value.floatingRate / 100 / 12;
-    const totalBulan = 12;
-    const angsuranBulanan =
-      (sisaPokok * rate) /
-      (1 -
-        Math.pow(1 + rate, -((inputs.value.tenor - tahun + 1) * totalBulan)));
+  try {
+    for (let tahun = 1; tahun <= inputs.value.tenor; tahun++) {
+      const rate =
+        tahun <= inputs.value.fixedTenor
+          ? inputs.value.fixedRate / 100 / 12
+          : tahun <= inputs.value.fixedTenor + inputs.value.floatingTenor
+          ? inputs.value.floatingRate / 100 / 12
+          : inputs.value.floatingRate / 100 / 12;
+      const totalBulan = 12;
+      const angsuranBulanan =
+        (sisaPokok * rate) /
+        (1 -
+          Math.pow(1 + rate, -((inputs.value.tenor - tahun + 1) * totalBulan)));
 
-    for (let i = 0; i < totalBulan; i++) {
-      const angsuranBunga = sisaPokok * rate;
-      const angsuranPokok = angsuranBulanan - angsuranBunga;
-      sisaPokok -= angsuranPokok;
+      for (let i = 0; i < totalBulan; i++) {
+        const angsuranBunga = sisaPokok * rate;
+        const angsuranPokok = angsuranBulanan - angsuranBunga;
+        sisaPokok -= angsuranPokok;
 
-      hasil.value.push({
-        Tahun: tahun,
-        Bulan: bulan++,
-        AngsuranBulanan: angsuranBulanan.toFixed(0),
-        AngsuranBunga: angsuranBunga.toFixed(0),
-        AngsuranPokok: angsuranPokok.toFixed(0),
-        SisaPokok: sisaPokok.toFixed(0),
-      });
+        hasil.value.push({
+          Tahun: tahun,
+          Bulan: bulan++,
+          AngsuranBulanan: angsuranBulanan.toFixed(0),
+          AngsuranBunga: angsuranBunga.toFixed(0),
+          AngsuranPokok: angsuranPokok.toFixed(0),
+          SisaPokok: sisaPokok.toFixed(0),
+        });
+      }
     }
+  } catch (error) {
+    console.error(error);
   }
+
+  isCalculating.value = false;
+
+  await nextTick();
+  showTable();
+};
+
+const showTable = () => {
+  hasilBarisRefs.value.forEach((el, index) => {
+    setTimeout(() => {
+      el.classList.remove("opacity-0");
+      el.classList.remove("leading-0");
+      el.classList.remove("border-0");
+      el.classList.add("not-last:border-b");
+    }, index * 50);
+  });
 };
 </script>
 
 <template>
-  <div>
-    <header>
-      <h2>Kalkulator KPR</h2>
-    </header>
-    <section>
-      <form @submit.prevent="hitungKPR">
-        <label>
-          Harga Property:
-          <input
-            v-model="formattedHargaProperty"
-            @input="updateValue('hargaProperty', $event)"
-          />
-        </label>
-        <label>
-          DP:
-          <input v-model="formattedDp" @input="updateValue('dp', $event)" />
-        </label>
-        <label>
-          Tenor (tahun):
-          <input
-            v-model="formattedTenor"
-            @input="updateValue('tenor', $event)"
-          />
-        </label>
-        <label>
-          Fixed Rate (%):
-          <input
-            v-model="inputs.fixedRate"
-            type="text"
-            @input="formatRateInput('fixedRate', $event)"
-          />
-        </label>
-        <label>
-          Fixed Tenor (tahun):
-          <input
-            v-model="formattedFixedTenor"
-            @input="updateValue('fixedTenor', $event)"
-          />
-        </label>
-        <label>
-          Floating Rate (%):
-          <input
-            v-model="inputs.floatingRate"
-            type="text"
-            @input="formatRateInput('floatingRate', $event)"
-          />
-        </label>
-        <label>
-          Floating Tenor (tahun):
-          <input
-            v-model="formattedFloatingTenor"
-            @input="updateValue('floatingTenor', $event)"
-          />
-        </label>
-        <button type="submit">Hitung</button>
-      </form>
-    </section>
+  <div class="container mx-auto">
+    <section
+      class="flex flex-col lg:flex-row gap-4 mt-6 lg:mt-10 mb-12 lg:mb-24"
+    >
+      <!-- Input Form -->
+      <div>
+        <div
+          class="sticky top-6 lg:top-10 shadow-lg outline outline-black/5 bg-white rounded-2xl p-4"
+        >
+          <h2 class="text-2xl font-bold mb-10">Simulasi KPR</h2>
+          <form class="flex flex-col gap-6" @submit.prevent="hitungKPR">
+            <!-- Harga Properti -->
+            <div>
+              <label
+                class="flex flex-col sm:flex-row sm:justify-between items-center gap-2"
+              >
+                <span class="w-full sm:w-44 font-semibold">
+                  Harga Properti
+                </span>
+                <div class="relative border-none rounded-none w-full sm:w-60">
+                  <input
+                    id="inputHargaProperty"
+                    v-model="formattedHargaProperty"
+                    class="border border-gray-300 rounded-md py-2 px-4 w-full sm:w-60"
+                    autofocus
+                    @input="updateValue('hargaProperty', $event)"
+                  />
+                </div>
+              </label>
+            </div>
 
-    <section>
-      <table v-if="hasil.length">
-        <thead>
-          <tr>
-            <th>Tahun</th>
-            <th>Bulan</th>
-            <th>Angsuran Bulanan</th>
-            <th>Angsuran Bunga</th>
-            <th>Angsuran Pokok</th>
-            <th>Sisa Pokok</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(item, index) in hasil" :key="index">
-            <td>{{ item.Tahun }}</td>
-            <td>{{ item.Bulan }}</td>
-            <td>{{ formatCurrency(item.AngsuranBulanan) }}</td>
-            <td>{{ formatCurrency(item.AngsuranBunga) }}</td>
-            <td>{{ formatCurrency(item.AngsuranPokok) }}</td>
-            <td>{{ formatCurrency(item.SisaPokok) }}</td>
-          </tr>
-        </tbody>
-      </table>
+            <!-- DP -->
+            <div>
+              <label
+                class="flex flex-col sm:flex-row sm:justify-between items-center gap-2"
+              >
+                <span class="w-full sm:w-44 font-semibold"> DP </span>
+                <div class="relative border-none rounded-none w-full sm:w-60">
+                  <input
+                    id="inputDP"
+                    v-model="formattedDp"
+                    class="border border-gray-300 rounded-md py-2 px-4 w-full sm:w-60"
+                    autofocus
+                    @input="updateValue('dp', $event)"
+                  />
+                </div>
+              </label>
+            </div>
+
+            <!-- Tenor -->
+            <div>
+              <label
+                class="flex flex-col sm:flex-row sm:justify-between items-center gap-2"
+              >
+                <span class="w-full sm:w-44 font-semibold"> Tenor </span>
+                <div class="relative border-none rounded-none w-full sm:w-60">
+                  <input
+                    id="inputTenor"
+                    v-model="formattedTenor"
+                    class="border border-gray-300 rounded-md py-2 px-4 w-full sm:w-60"
+                    autofocus
+                    @input="formattte('tenor', $event)"
+                  />
+                  <span
+                    class="absolute right-0 top-1.5 px-4 py-1 border-l border-gray-200 text-gray-500 text-sm"
+                  >
+                    Tahun
+                  </span>
+                </div>
+              </label>
+            </div>
+
+            <!-- Fixed Rate -->
+            <div class="flex flex-col sm:flex-row items-center gap-2">
+              <label class="flex w-full" for="inputFixedRate">
+                <span class="w-full sm:w-44 font-semibold"> Fixed Rate </span>
+              </label>
+              <div class="flex items-center gap-4 sm:w-64 w-full">
+                <div class="relative grow sm:w-28">
+                  <input
+                    id="inputFixedRate"
+                    v-model="inputs.fixedRate"
+                    class="border border-gray-300 rounded-md py-2 px-4 w-full"
+                    @input="formatRateInput('fixedRate', $event)"
+                  />
+                  <label for="inputFixedRate">
+                    <span
+                      class="absolute right-0 top-1.5 px-4 py-1 border-l border-gray-200 text-gray-500 text-sm"
+                    >
+                      %
+                    </span>
+                  </label>
+                </div>
+
+                <div class="relative grow sm:w-28">
+                  <input
+                    id="inputFixedTenor"
+                    v-model="formattedFixedTenor"
+                    class="border border-gray-300 rounded-md py-2 px-4 w-full"
+                    @input="updateValue('fixedTenor', $event)"
+                  />
+                  <label for="inputFixedTenor">
+                    <span
+                      class="absolute right-0 top-1.5 px-4 py-1 border-l border-gray-200 text-gray-500 text-sm"
+                    >
+                      Tahun
+                    </span>
+                  </label>
+                </div>
+              </div>
+            </div>
+
+            <!-- Floating Rate -->
+            <div class="flex flex-col sm:flex-row items-center gap-2">
+              <label class="flex w-full" for="inputFloatingRate">
+                <span class="w-full sm:w-44 font-semibold">Floating Rate</span>
+              </label>
+              <div class="flex items-center gap-4 sm:w-64 w-full">
+                <div class="relative grow sm:w-28">
+                  <input
+                    id="inputFloatingRate"
+                    v-model="inputs.floatingRate"
+                    class="border border-gray-300 rounded-md py-2 px-4 w-full"
+                    @input="formatRateInput('floatingRate', $event)"
+                  />
+                  <label for="inputFloatingRate">
+                    <span
+                      class="absolute right-0 top-1.5 px-4 py-1 border-l border-gray-200 text-gray-500 text-sm"
+                    >
+                      %
+                    </span>
+                  </label>
+                </div>
+
+                <div class="relative grow sm:w-28">
+                  <input
+                    id="inputFloatingTenor"
+                    v-model="formattedFloatingTenor"
+                    class="border border-gray-300 rounded-md py-2 px-4 w-full"
+                    @input="updateValue('floatingTenor', $event)"
+                  />
+                  <label for="inputFloatingTenor">
+                    <span
+                      class="absolute right-0 top-1.5 px-4 py-1 border-l border-gray-200 text-gray-500 text-sm"
+                    >
+                      Tahun
+                    </span>
+                  </label>
+                </div>
+              </div>
+            </div>
+
+            <button
+              type="submit"
+              class="px-6 py-3.5 text-base font-medium text-white inline-flex items-center justify-center bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:outline-none focus:ring-cyan-200 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              <svg
+                v-if="isCalculating"
+                aria-hidden="true"
+                role="status"
+                class="inline w-4 h-4 me-3 text-white animate-spin"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="#E5E7EB"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentColor"
+                />
+              </svg>
+              Hitung
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <!-- Results Table -->
+      <section class="rounded-lg overflow-y-hidden">
+        <table
+          v-if="hasil.length"
+          class="border border-gray-100 table-auto w-full text-md text-left rtl:text-right text-gray-500 dark:text-gray-400"
+        >
+          <thead
+            class="text-gray-700 uppercase dark:text-gray-400 border-y border-gray-300 bg-gray-200"
+          >
+            <tr>
+              <th class="px-6 py-3 text-center">Tahun</th>
+              <th class="px-6 py-3 text-center">Bulan</th>
+              <th class="px-6 py-3 text-center">Angsuran Bulanan</th>
+              <th class="px-6 py-3 text-center">Angsuran Bunga</th>
+              <th class="px-6 py-3 text-center">Angsuran Pokok</th>
+              <th class="px-6 py-3 text-center">Sisa Pokok</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              class="border-b border-gray-200 dark:border-gray-700 hover:bg-gray-50 bg-gray-100 *:px-6 *:py-2"
+            >
+              <td colspan="5">Pinjaman</td>
+              <td class="text-right">
+                {{ formatCurrency(inputs.hargaProperty - inputs.dp) }}
+              </td>
+            </tr>
+            <tr
+              v-for="(item, index) in hasil"
+              :ref="(el) => (hasilBarisRefs[index] = el)"
+              :key="index"
+              class="border-gray-200 dark:border-gray-700 hover:bg-gray-50 *:px-6 *:py-2"
+              :class="{
+                'bg-gray-100': item.Tahun % 2 === 0,
+                'transition-[opacity] duration-200 ease-in-out': true,
+                'opacity-0 leading-0 border-0 p-0': true,
+              }"
+            >
+              <td class="text-center">{{ item.Tahun }}</td>
+              <td class="text-center">{{ item.Bulan }}</td>
+              <td class="text-right">
+                {{ formatCurrency(item.AngsuranBulanan) }}
+              </td>
+              <td class="text-right">
+                {{ formatCurrency(item.AngsuranBunga) }}
+              </td>
+              <td class="text-right">
+                {{ formatCurrency(item.AngsuranPokok) }}
+              </td>
+              <td class="text-right">
+                {{ formatCurrency(item.SisaPokok) }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
     </section>
   </div>
 </template>
