edbee703e67df67a83c73c7ca39c1dfc5ff426f9
Wildan Novaldi <wildanv10@gmail.com>
2025-03-29 15:33:44 +0700

    feat: install tailwindcss@4.0.14

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ["@nuxt/eslint"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["~/assets/css/main.css"],
   app: {
     head: {
-      title: 'Simulasi KPR',
+      title: "Simulasi KPR",
       htmlAttrs: {
-        lang: 'id',
+        lang: "id",
       },
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
       meta: [
-        { name: 'description', content: 'Gunakan Kalkulator KPR online untuk menghitung angsuran rumah dengan mudah. Simulasi Kredit Pemilikan Rumah (KPR) berdasarkan harga properti, DP, suku bunga tetap & mengambang, serta tenor pinjaman.' }
+        {
+          name: "description",
+          content:
+            "Gunakan Kalkulator KPR online untuk menghitung angsuran rumah dengan mudah. Simulasi Kredit Pemilikan Rumah (KPR) berdasarkan harga properti, DP, suku bunga tetap & mengambang, serta tenor pinjaman.",
+        },
       ],
-    }
-  }
-})
+    },
+  },
+});
